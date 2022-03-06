6ba14e90adcf50e06bd2e0c9ab2cc410476d65d3
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2022-03-07 02:06:46 +0900

    コメント追加

--- app/src/main/java/com/example/recycleview_sample/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recycleview_sample/adapter/ItemAdapter.kt
@@ -7,15 +7,21 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.model.Affirmation
 import com.example.recycleview_sample.R
 
+// MainActivity]の[RecyclerView]用アダプタ Affirmation]データオブジェクトを表示
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Affirmation>
 ) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
+    // 各データ項目に対応するビューへの参照を提供
+    // 複雑なデータ項目には、項目ごとに複数のビューが必要な場合があり
+    // ビューホルダーでデータアイテムのすべてのビューにアクセスできるようにする
+    // 各データ項目は単なる Affirmation オブジェクトである
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
+    // 新規ビューの作成（レイアウトマネージャから呼び出される）
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
@@ -24,9 +30,11 @@ class ItemAdapter(
         return ItemViewHolder(adapterLayout)
     }
 
+    // データセットのサイズを返す（レイアウトマネージャによって起動される）
     override fun getItemCount() = dataset.size
 
 
+    //  ビューの内容を置き換える（レイアウトマネージャから呼び出される）
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text =  context.resources.getString(item.stringResourceId)
